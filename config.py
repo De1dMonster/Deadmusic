ee5d0e661b1dddea244fe96f9d024a48694594ca
De1dMonster <144820063+De1dMonster@users.noreply.github.com>
2023-11-07 22:00:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SAIFDEAD/DEADMUSIC",
+    "https://github.com/De1dMonster/Deadmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
